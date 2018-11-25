49c7cdcf079a448e4fc822d4904720baaa6a135d
Reza Shah <33800875+iamrezashah@users.noreply.github.com>
2018-11-25 19:28:16 +0330

    Update order.py

--- order.py
+++ order.py
@@ -7,32 +7,34 @@ if len(t) % 2 == 0:
             opening_list.append(i+1)
         else:
             closing_list.append(i+1)
+    
+    pairs = []
+    z = 2 * int(len(t)/2)
+
+    for diff in range(1, z, 2):
+        if len(pairs) != 0:
+            for n in pairs:
+                ni = n[0]
+                nj = n[1]
+                if ni in opening_list and nj in closing_list:
+                    opening_list.remove(ni)
+                    closing_list.remove(nj)
+
+        if len(opening_list) != 0:
+            for i in opening_list:
+                for j in closing_list:
+                    print(i, j)
+                    x = j - i
+                    if x == diff:
+                        pairs.append([i, j])
+                        break
+                print('--- end of j')
+            print('=== end of i')
+
+
+    print(pairs)
 else:
-    print('Error !')
+    print('Input Error !')
 
 
-pairs = []
-z = 2 * int(len(t)/2)
 
-for diff in range(1, z, 2):
-    if len(pairs) != 0:
-        for n in pairs:
-            ni = n[0]
-            nj = n[1]
-            if ni in opening_list and nj in closing_list:
-                opening_list.remove(ni)
-                closing_list.remove(nj)
-
-    if len(opening_list) != 0:
-        for i in opening_list:
-            for j in closing_list:
-                print(i, j)
-                x = j - i
-                if x == diff:
-                    pairs.append([i, j])
-                    break
-            print('--- end of j')
-        print('=== end of i')
-
-
-print(pairs)
